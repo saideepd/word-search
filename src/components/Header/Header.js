caf6098a8522a3e49b03bf50b9af95e199c539c0
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2021-12-19 22:21:59 +0530

    Initial Commit with basic structure

---
+++ src/components/Header/Header.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import './Header.css';
+import { createTheme, MenuItem, TextField, ThemeProvider } from '@mui/material';
+import categories from '../../data/category'; 
+
+const Header = ({ category, setCategory, word, setWord }) => {
+
+    const darkTheme = createTheme({
+        palette: {
+            primary: {
+                main: "#fff",
+            },
+            mode: 'dark',
+        },
+      });
+
+    return (
+        <div className="header">
+            <span className="title">{ word ? word : "Word Search" }</span>
+            <div className="inputs">
+                <ThemeProvider theme={darkTheme}>
+                    <TextField 
+                        className="search" 
+                        label="Search a Word" 
+                        variant="standard" 
+                        value={word}
+                        onChange={(e) => setWord(e.target.value)}
+                    />
+                        
+                    <TextField
+                        select
+                        className="select"
+                        label="Language"
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                        variant="standard"
+                    >
+                        {categories.map((option) => (
+                            <MenuItem key={option.label} value={option.label}>
+                                { option.value }
+                            </MenuItem>
+                        ))}
+                    </TextField>
+                </ThemeProvider>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/data/category.js
@@ -0,0 +1,21 @@
+const categories = [
+    { value: "English (US)", label: "en" },
+    { value: "English (UK)", label: "en-uk" },
+    { value: "Hindi", label: "hi" },
+    { value: "Spanish", label: "es" },
+    { value: "French", label: "fr" },
+    { value: "Japanese", label: "ja" },
+    { value: "Czech", label: "cs" },
+    { value: "Dutch", label: "nl" },
+    { value: "Slovak", label: "sk" },
+    { value: "Russian", label: "ru" },
+    { value: "German", label: "de" },
+    { value: "Italian", label: "it" },
+    { value: "Korean", label: "ko" },
+    { value: "Brazilian Portuguese", label: "pt-BR" },
+    { value: "Arabic", label: "ar" },
+    { value: "Turkish", label: "tr" },
+];
+
+
+export default categories;
