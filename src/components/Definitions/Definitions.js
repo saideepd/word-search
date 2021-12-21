93445c9cbdc4a1d9d1faf0ecbe331750f28a2125
Saideep Dicholkar <dicholkarsaideep@gmail.com>
2021-12-22 00:32:42 +0530

    Added logic to display definitions & synonyms & related styles

--- src/components/Definitions/Definitions.js
+++ src/components/Definitions/Definitions.js
@@ -1,10 +1,40 @@
 import React from 'react'
 import './Definitions.css'
 
-const Definitions = ({word, meanings, category}) => {
+const Definitions = ({ word, meanings, category }) => {
     return (
-        <div>
-            Hello
+        <div className="meanings">
+            {word === "" ? (<span className="subTitle">Start by typing a word in Search</span>)
+                : (
+                    meanings.map((mean) =>
+                        mean.meanings.map((item) =>
+                            item.definitions.map((def) => (
+                                <div
+                                    className="singleMean"
+                                    style={{ backgroundColor: 'white', color: 'black' }}
+                                >
+                                    <b>{def.definition}</b>
+                                    <hr style={{ backgroundColor: "black", width: "100%" }} />
+
+                                    {def.example && (
+                                        <span>
+                                            <b>Example : </b>
+                                            {def.example}
+                                        </span>
+                                    )}
+                                    <br />
+                                    {def.synonyms && (
+                                        <span>
+                                            <b>Synonyms : </b>
+                                            {def.synonyms.map((s) => s).join(', ')}
+                                        </span>
+                                    )}
+                                </div>
+                            ))
+                        )
+                    )
+                )
+            }
         </div>
     )
 }
